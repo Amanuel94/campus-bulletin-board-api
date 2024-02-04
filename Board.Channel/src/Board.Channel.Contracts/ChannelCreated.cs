80f3c8c22d99b876c50e3e0c37ab0c92c7bf1e70
= <amanuel.tewodros@a2sv.org>
2024-02-04 15:19:41 +0300

    document channel service contracts

--- Board.Channel/src/Board.Channel.Contracts/ChannelCreated.cs
+++ Board.Channel/src/Board.Channel.Contracts/ChannelCreated.cs
@@ -1,4 +1,5 @@
-﻿namespace Board.Channel.Contracts;
+﻿// Purpose: This file contains the ChannelCreated class which is used to represent the event of a channel being created.
+namespace Board.Channel.Contracts;
 public class ChannelCreated
 {
     public Guid Id {get; set;}
--- Board.Channel/src/Board.Channel.Contracts/ChannelDeleted.cs
+++ Board.Channel/src/Board.Channel.Contracts/ChannelDeleted.cs
@@ -1,4 +1,5 @@
-﻿namespace Board.Channel.Contracts;
+﻿// Purpose: This file contains the ChannelDeleted class which is used to represent the event of a channel being deleted.
+namespace Board.Channel.Contracts;
 public class ChannelDeleted
 {
     public Guid Id;
